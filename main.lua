2960e9eb170ca61d0d277c3da198ccf546f19520
Robert Woods <robbiewoods05@gmail.com>
2016-01-25 01:44:48 +0000

    Now able to draw tiles

--- main.lua
+++ main.lua
@@ -1,86 +1,109 @@
 --18 up, 25 across
-
---[[function CreateQuads(quadArray, tileSize, tileset, rows, cols, tileKey)
+function CreateQuads(quadArray, tileSize, tileset, rows, cols, tileKey)
     local index, xPos, yPos = 1, 0, 0
 
     for _ = 1, rows do
         for __ = 1, cols do
-            quadArray[tileKey[index] = love.graphics.newQuad(xPos, yPos, tileSize, tileSize, tileset:getWidth(), tileset:getHeight())
+            quadArray[tileKey[index]] = love.graphics.newQuad(xPos, yPos, tileSize, tileSize, tileset:getWidth(), tileset:getHeight())
             xPos = xPos + tileSize
             index = index + 1
         end
         xPos = 0
         yPos = yPos + tileSize
     end
-end]]
+end
+
+function AddTile(tilemap, mouseX, mouseY, tilekey)
+    yTile, xTile = math.floor(love.mouse.getY() / 32)  + 1, math.floor(love.mouse.getX() / 32) + 1
+    tilemap[yTile][xTile] = tilekey
+end
+
+function RemoveTile(tilemap, mouseX, mouseY)
+    yTile, xTile = math.floor(love.mouse.getY() / 32)  + 1, math.floor(love.mouse.getX() / 32) + 1
+    tilemap[yTile][xTile] = nil
+end
 
 function love.load(args)
-    if arg[#arg] == "-debug" then require("mobdebug").start() end
+    love.mouse.setVisible(false)
+
+    --Zerobrane debugging
+    --if arg[#arg] == "-debug" then require("mobdebug").start() end
+
+    -- Loads the tileset
     tileset = love.graphics.newImage('countryside.png')
-    x, y = nil, nil
+    x, y, tileSize = nil, nil, 32
+
+    -- Index of the current tile to be drawn, corresponds with tile key
     currentTile = 1
-    drawTile = false
+
+    -- Table to hold string representation of tiles
     TileMap = {}
-    TileQuads = {}
+
     rows, cols = 2, 2
+
+    -- Table with tile keys to allow increment if tiles
     tileKey = {' ', '#', '*', '^'}
 
-    TileQuads[' '] = love.graphics.newQuad(0, 0, 32, 32, tileset:getWidth(), tileset:getHeight())
-    TileQuads['#'] = love.graphics.newQuad(0, 32, 32, 32, tileset:getWidth(), tileset:getHeight())
+    -- Table of tile quads
+    TileQuads = {}
+    CreateQuads(TileQuads, tileSize, tileset, rows, cols, tileKey)
 
-    love.mouse.setVisible(false)
-    --love.keyboard.setKeyRepeat(false)
-    --CreateQuads(TileQuads, 32, tileset, rows, cols, tileKey)
+    -- Iterate through array, create another table for each y, for a 2D array
     for i = 1,18 do
         TileMap[i] = {}
     end
-
-    TileMap[18][25] = '#'
-    TileMap[1][1] = ' '
 end
 
 function love.update(dt)
-    x, y = love.mouse.getX(), love.mouse.getY()
-
-
-    mouseX, mouseY = 0, 0
-
-    if love.mouse.isDown(1) then
-        mouseX, mouseY = love.mouse.getX() / 32, love.mouse.getY() / 32
+    if love.mouse.getX() <= 0 then
+        xPos = 0
+    elseif love.mouse.getX() >= love.graphics.getWidth() - tileSize then
+        xPos = love.graphics.getWidth() - 32
+    elseif love.mouse.getY() <= 0 then
+        yPos = 0
+    elseif love.mouse.getY() >= love.graphics.getHeight() - tileSize then
+        yPos = love.graphics.getHeight() - tileSize
+    else
+        xPos, yPos = love.mouse.getX(), love.mouse.getY()
     end
+
 end
 
 function love.keypressed(key, isrepeat)
-      --if not isrepeat then
-          if key == "right" then
-              if currentTile < rows * cols then
-                  currentTile = currentTile + 1
-              else
-                  currentTile = 1
-              end
-          elseif key == "left" then
-              if currentTile > 1 then
-                  currentTile = currentTile - 1
-              else
-                  currentTile = rows * cols
-              end
-          end
-      --end
+    -- Change tiles, wraps around if end is reached.
+    if key == "right" then
+      if currentTile < rows * cols then
+          currentTile = currentTile + 1
+      else
+          currentTile = 1
+      end
+  elseif key == "left" then
+      if currentTile > 1 then
+          currentTile = currentTile - 1
+      else
+          currentTile = rows * cols
+      end
+  end
+end
+
+function love.mousepressed(x, y, button, istouch)
+   if button == 1 then -- the primary button
+      AddTile(TileMap, x, y, tileKey[currentTile])
+  elseif button == 2 then
+      RemoveTile(TileMap, x, y)
+  end
 end
 
 function love.draw()
-        for yAxis = 1, 18 do
-            for xAxis = 1, 25 do
-                    if TileMap[yAxis][xAxis] ~= nil then
-                        index = TileMap[yAxis][xAxis]
-                        love.graphics.draw(tileset, TileQuads[index], (xAxis - 1) * 32, (yAxis - 1) * 32)
-                    end
+    -- Draws tile map, black if nil
+    for yAxis = 1, 18 do
+        for xAxis = 1, 25 do
+            if TileMap[yAxis][xAxis] ~= nil then
+                index = TileMap[yAxis][xAxis]
+                love.graphics.draw(tileset, TileQuads[index], (xAxis - 1) * 32, (yAxis - 1) * 32)
             end
         end
+    end
 
-    love.graphics.draw(tileset, TileQuads[' '], x, y)
-    love.graphics.print("X: " .. tostring(math.floor(mouseX)), 0, 0)
-    love.graphics.print("Y: " .. tostring(math.floor(mouseY)), 0, 20)
-    love.graphics.print("Current tile key: " .. tileKey[currentTile], 0, 30)
-    love.graphics.print("Current tile index: " .. currentTile, 0, 40)
+    love.graphics.draw(tileset, TileQuads[tileKey[currentTile]], xPos, yPos)
 end
